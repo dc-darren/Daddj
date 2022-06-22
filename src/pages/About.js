184fe3170b6d442fb7affbf74bb13a621d509fee
dc-darren <darren.denisson@gmail.com>
2022-06-22 15:19:18 +0700

    Added responsive margin left to features

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,7 +13,7 @@ const About = () => {
         </div>
         <div className="daddj-texts">
           <h2>Features</h2>
-          <ul className="list-disc ml-3">
+          <ul className="list-disc ml-3 sm:ml-4 lg:ml-5">
             <li>Get a random dad joke</li>
             <li>Search for dad jokes by a keyword</li>
             <li>Obtain the image version of a specific dad joke</li>
