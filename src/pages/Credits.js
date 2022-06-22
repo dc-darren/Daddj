deb51aaa26360d589391eb13813e6066f4ddb344
dc-darren <darren.denisson@gmail.com>
2022-06-22 15:58:08 +0700

    Updated Credits page images path

--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -11,7 +11,7 @@ const Credits = () => {
           <h2>Public API</h2>
           <a href="https://icanhazdadjoke.com/" className="daddj-link">
             <div className="daddj-img-link">
-              <img src="/credits/icanhazdadjoke.svg" alt="[img]" className="daddj-img" />
+              <img src="./credits/icanhazdadjoke.svg" alt="[img]" className="daddj-img" />
               <p>icanhazdadjoke</p>
             </div>
           </a>
@@ -20,13 +20,13 @@ const Credits = () => {
           <h2>Frameworks</h2>
           <a href="https://reactjs.org/" className="daddj-link">
             <div className="daddj-img-link">
-              <img src="/credits/react.svg" alt="[img]" className="daddj-img" />
+              <img src="./credits/react.svg" alt="[img]" className="daddj-img" />
               <p>React</p>
             </div>
           </a>
           <a href="https://tailwindcss.com/" className="daddj-link">
             <div className="daddj-img-link">
-              <img src="/credits/tailwindcss.svg" alt="[img]" className="daddj-img" />
+              <img src="./credits/tailwindcss.svg" alt="[img]" className="daddj-img" />
               <p>TailwindCSS</p>
             </div>
           </a>
@@ -35,7 +35,7 @@ const Credits = () => {
           <h2>GitHub Repository</h2>
           <a href="https://github.com/dc-darren/daddj" className="daddj-link">
             <div className="daddj-img-link">
-              <img src="/credits/github.svg" alt="[img]" className="daddj-img" />
+              <img src="./credits/github.svg" alt="[img]" className="daddj-img" />
               <p>dc-darren/daddj</p>
             </div>
           </a>
